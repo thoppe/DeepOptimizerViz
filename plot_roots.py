300afda56bf230d8c6fa193136a5ab754cb67478
Travis Hoppe <travis.hoppe@gmail.com>
2018-09-28 17:14:38 -0400

    Improve plotting

--- plot_roots.py
+++ plot_roots.py
@@ -14,8 +14,8 @@ os.system(f'mkdir -p {figure_dest}')
 
 assert(os.path.exists(f_h5))
 
-cutoff = 200
-total_frames = 8000
+cutoff = 2000
+total_frames = 20000
 trail_iterations = 1000
 
 with h5py.File(f_h5,'r') as h5:
@@ -40,10 +40,8 @@ def plot_frame(k):
     plt.cla()
 
     
-    print(f"Plotting frame {k}")
-
-    x_pts = x[:,k-trail_iterations:k]
-    y_pts = y[:,k-trail_iterations:k]
+    x_pts = x[:,k:k+trail_iterations]
+    y_pts = y[:,k:k+trail_iterations]
 
     idx = (
         np.isclose(x_pts.sum(axis=1),0,1e-2) &
@@ -52,13 +50,14 @@ def plot_frame(k):
 
     x_pts = x_pts[~idx]
     y_pts = y_pts[~idx]
-    
+
+    print(f"Plotting frame {k}, {x_pts.shape}")
 
     pts_N = x_pts.ravel().size
     #alpha = np.logspace(1, 2, x_pts.shape[1]).tolist()*x_pts.shape[0]
     alpha = np.linspace(0,1, x_pts.shape[1]).tolist()*x_pts.shape[0]
     alpha = np.array(alpha)
-    alpha /= alpha.max()
+    #alpha /= alpha.max()
 
     rgba_colors = np.zeros((pts_N,4))
     rgba_colors[:,0] = 0.4
@@ -78,6 +77,6 @@ def plot_frame(k):
 
 plt.figure(figsize=(6,6))
 
-for k in tqdm(range(1000, 2000, 10)):
+for k in tqdm(range(0, 8000, 10)):
     plot_frame(k)
 
